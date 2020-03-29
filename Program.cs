1dc69cd42a5281d69416ef9027a7addc3feb0319
Muralidharan Subramanian <muralidharan.s@hotmail.com>
2020-03-29 11:22:09 -0400

    First commit from VS Code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StringUniqueCharacters
+{
+    class Program
+    {
+        private static bool isuniquechars (string strinput)
+        {
+            // 256 chars fo
+            bool[] bchars = new bool[256];
+
+            foreach(char c in strinput)
+            {
+                if (bchars[c]== true)
+                {
+                    return false;
+                }
+                else
+                {
+                        bchars[c]= true;
+                }
+
+            }
+
+            return true;
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isuniquechars("Muralidharan"));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
